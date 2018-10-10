746f2058ddf320a80899e0d55a1e2b1fc0d75d51
Anna Del Prete <anna@ustwo.com>
2018-10-10 20:23:01 +0100

    Fix Search

--- src/actions/SearchAction.js
+++ src/actions/SearchAction.js
@@ -6,20 +6,22 @@ const setQuery = (query, results) => ({
     results
 });
 
-export const startQuery = (query = '') => {
+export const startQuery = (query) => {
+    console.log('query', query)
     return (dispatch) => {
+        const books = []
         return search(query).then((results) => {
-            const books = []
-            if(results.length > 0) {
-                results.map((result) => {
-                    books.push({
-                        title : result.title,
-                        id: result.id,
-                        imageLinks: result.imageLinks
-                    })
+            console.log('results', results)
+            results.map((result) => {
+                books.push({
+                    title : result.title,
+                    id: result.id,
+                    imageLinks: result.imageLinks
                 })
-                return dispatch(setQuery(query, books));
-            }
+            })
+            return dispatch(setQuery(query, books));
+        }).catch((e) => {
+            return dispatch(setQuery(query, []));
         })
     }
 }
