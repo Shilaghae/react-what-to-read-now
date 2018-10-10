b018c2aaf0b7cbce101db4fd51b8dcc658142032
Anna Del Prete <anna@ustwo.com>
2018-10-10 17:18:11 +0100

    Remove unused code

--- src/reducers/BooksReducer.js
+++ src/reducers/BooksReducer.js
@@ -2,10 +2,6 @@ const booksReducerDefaultState = [];
 
 export default (state = booksReducerDefaultState, action) => {
   switch (action.type) {
-    case 'SEARCH_BOOKS':
-      return [
-        ...action.books
-      ]
     case 'SET_BOOKS':
       return [
         ...action.books
